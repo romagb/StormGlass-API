80011684e880b028d577641546df819cd8c5b91b
IZenerO <romikgerus14@gmail.com>
2021-03-31 13:13:38 +0300

    Implemented Twisted agent script for StormGlass API.

---
+++ stormglass_req.py
@@ -0,0 +1,92 @@
+from pprint import pformat
+import sys
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.internet.protocol import Protocol
+from twisted.web.client import Agent, readBody
+from twisted.web.http_headers import Headers
+
+import urlparse
+from urllib import urlencode
+
+DEFAULT_LOCATIONS = {
+    'Kyiv': {
+        'lat': 50.45466,
+        'lng': 30.5238,
+        'params': sys.argv[1]
+    },
+    'Lviv': {
+        'lat': 49.842957,
+        'lng': 24.031111,
+        'params': sys.argv[1]
+    },
+    'Austin': {
+        'lat': 30.26666,
+        'lng': -97.73333,
+        'params': sys.argv[1]
+    },
+    'New York': {
+        'lat': 40.73061,
+        'lng': -73.935242,
+        'params': sys.argv[1]
+    },
+    'Paris': {
+        'lat': 48.86471,
+        'lng': 2.34901,
+        'params': sys.argv[1]
+    }
+}
+
+
+class BeginningPrinter(Protocol):
+    def __init__(self, finished):
+        self.finished = finished
+        self.remaining = 1024 * 10
+
+    def dataReceived(self, bytes):
+        if self.remaining:
+            display = bytes[:self.remaining]
+            print 'Some data received:'
+            print display
+            self.remaining -= len(display)
+
+    def connectionLost(self, reason):
+        print 'Finished receiving body:', reason.getErrorMessage()
+        self.finished.callback(None)
+
+
+def cbRequest(response):
+    print 'Response version:', response.version
+    print 'Response code:', response.code
+    print 'Response phrase:', response.phrase
+    print 'Response headers:'
+    print pformat(list(response.headers.getAllRawHeaders()))
+    finished = Deferred()
+    response.deliverBody(BeginningPrinter(finished))
+    return finished
+
+
+def link_generator(default_link, dictionary):
+    url_parts = list(urlparse.urlparse(default_link))
+    query = dict(urlparse.parse_qsl(url_parts[3]))
+    query.update(dictionary)
+    url_parts[4] = urlencode(query)
+    url = urlparse.urlunparse(url_parts)
+    return url
+
+
+start_url = 'https://api.stormglass.io/v2/weather/point'
+url_list = []
+for key in DEFAULT_LOCATIONS:
+    url_list.append(link_generator(start_url, DEFAULT_LOCATIONS[key]))
+
+agent = Agent(reactor)
+for link in url_list:
+    d = agent.request(
+        'GET',
+        link,
+        Headers({'Authorization': ['8d8789f0-8db4-11eb-a9f7-0242ac130002-8d878a68-8db4-11eb-a9f7-0242ac130002']}),
+        None)
+    d.addCallback(cbRequest)
+
+reactor.run()
